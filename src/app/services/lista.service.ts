8969ed57521ea8aeeebfeb7823a2ab01a02d0dc9
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-08 13:27:38 -0300

    Comantários no serviço

--- src/app/services/lista.service.ts
+++ src/app/services/lista.service.ts
@@ -7,9 +7,13 @@ import { Item } from './item';
   providedIn: 'root'
 })
 export class ListaService {
+  // json-server
+  // private readonly API = 'https://json-server-seven-alpha.vercel.app/lista';
 
-  private readonly API = 'https://json-server-seven-alpha.vercel.app/lista';
-  // private readonly API = 'https://json-server-vercel-pied.vercel.app/lista';
+  // json-server-vercel
+  private readonly API = 'https://json-server-vercel-pied.vercel.app/lista';
+
+  // localhost
   // private readonly API = 'http://127.0.0.1:3000/lista';
 
 
